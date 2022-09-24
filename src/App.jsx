59c6d76a2094967b04e44773a990d17085db6e44
Jochyd <mail>
2022-09-24 16:24:33 -0300

    realice el ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Hello from "./components/Hello";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
+  let saludo = 'My Friend'
   return (
     <>
  <HelloWorld></HelloWorld>
+    <Hello saludo ={saludo}></Hello>
     </>
   );
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = (props) => {
+    return (
+        <div>
+            <h2>Hello : {props.saludo}</h2>
+        </div>
+    );
+};
+
+export default Hello;
